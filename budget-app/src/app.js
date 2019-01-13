07c3861683023b25e649c709aa51645a6cc9c665
franciscomanuelmatos <franciscomanuelmatos@gmail.com>
2019-01-13 22:16:45 +0000

    Added new components: ExpenseListItem and ExpenseListFilters. Connected ExpenseList to redux store; filtering by expense text and removing an expense have been implemented

--- budget-app/src/app.js
+++ budget-app/src/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import configStore from './store/index';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 
 const store = configStore();
 
@@ -14,4 +17,16 @@ const jsx = (
   </Provider>
 );
 
+store.dispatch(addExpense({
+  description: 'Rent',
+  amount: 5000
+}));
+
+store.dispatch(addExpense({
+  description: 'Water Bill',
+  amount: 5000
+}));
+
 ReactDOM.render(jsx, document.getElementById('app'));
+
+
--- budget-app/src/components/ExpenseList.js
+++ budget-app/src/components/ExpenseList.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import ExpenseListFilters from './ExpenseListFilters';
+import { getVisibleExpenses } from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div>
-    Expense List
-    {props.expenses.length}
+    <h1>Expense List</h1>
+    <ExpenseListFilters />
+
+    {props.expenses.map((expense) => (
+      <ExpenseListItem expense={expense} key={expense.id} /> 
+    ))}
   </div>
 );
 
 const mapStateToProps = (state) => ({
-  expenses: state.expenses
+  expenses: getVisibleExpenses(state.expenses, state.filters)
 });
 
 export default connect(mapStateToProps)(ExpenseList);
