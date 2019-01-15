59ef87033228ebcddc87b9550863d65e2f53daa3
franciscomanuelmatos <franciscomanuelmatos@gmail.com>
2019-01-15 20:47:31 +0000

    Fixed indentation

--- budget-app/src/actions/filters.js
+++ budget-app/src/actions/filters.js
@@ -6,24 +6,24 @@ export const setTextFilter = (text = '') => ({
     text
   });
   
-  // SORT_BY_DATE
-  export const sortByDate = () => ({
-    type: SORT_BY_DATE
-  });
-  
-  // SORT_BY_AMOUNT
-  export const sortByAmount = () => ({
-    type: SORT_BY_AMOUNT
-  });
-  
-  // SET_START_DATE
-  export const setStartDate = (startDate) => ({
-    type: SET_START_DATE,
-    startDate
-  });
-  
-  // SET_END_DATE
-  export const setEndDate = (endDate) => ({
-    type: SET_END_DATE,
-    endDate
-  });
+// SORT_BY_DATE
+export const sortByDate = () => ({
+  type: SORT_BY_DATE
+});
+
+// SORT_BY_AMOUNT
+export const sortByAmount = () => ({
+  type: SORT_BY_AMOUNT
+});
+
+// SET_START_DATE
+export const setStartDate = (startDate) => ({
+  type: SET_START_DATE,
+  startDate
+});
+
+// SET_END_DATE
+export const setEndDate = (endDate) => ({
+  type: SET_END_DATE,
+  endDate
+});
