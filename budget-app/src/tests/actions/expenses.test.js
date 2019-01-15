c6f9411fb2fcd4e2744c85614bbad1b76eae43fe
franciscomanuelmatos <franciscomanuelmatos@gmail.com>
2019-01-15 20:47:55 +0000

    Wrote tests for the action generators

--- budget-app/src/tests/actions/expenses.test.js
+++ budget-app/src/tests/actions/expenses.test.js
@@ -1,2 +1,59 @@
 import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { ADD_EXPENSE, EDIT_EXPENSE, REMOVE_EXPENSE } from '../../actions/actionTypes';
 
+const id = '123abc';
+const testObj = {
+  description: 'Test expense',
+  note: '',
+  amount: 500,
+  createdAt: 1000
+}
+const defaultExpense = {
+  description: '',
+  note: '',
+  amount: 0,
+  createdAt: 0
+}
+
+test('Should setup remove action object', () => {
+  const action = removeExpense({ id });
+
+  expect(action).toEqual({
+    type: REMOVE_EXPENSE,
+    id
+  });
+});
+
+test('Should setup edit action object', () => {
+  const action = editExpense(id, testObj);
+
+  expect(action).toEqual({
+    type: EDIT_EXPENSE,
+    id,
+    updates: testObj
+  })
+})
+
+test('Should setup add action object with provided values', () => {
+  const action = addExpense(testObj);
+
+  expect(action).toEqual({
+    type: ADD_EXPENSE,
+    expense: {
+      ...testObj,
+      id: expect.any(String)
+    }
+  })
+})
+
+test('Should setup add action object with default values', () => {
+  const action = addExpense();
+
+  expect(action).toEqual({
+    type: ADD_EXPENSE,
+    expense: {
+      ...defaultExpense,
+      id: expect.any(String)
+    }
+  })
+})
--- budget-app/src/tests/actions/filters.test.js
+++ budget-app/src/tests/actions/filters.test.js
@@ -1,3 +1,53 @@
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../../actions/filters';
+import moment from 'moment';
 
+import { setEndDate, setStartDate, setTextFilter, sortByAmount, sortByDate } from '../../actions/filters';
+import { SET_END_DATE, SET_START_DATE, SET_TEXT_FILTER, SORT_BY_AMOUNT, SORT_BY_DATE } from '../../actions/actionTypes';
 
+test('Should setup a set text filter action with given parameters', () => {
+  const text = 'test';
+  const action = setTextFilter(text);
+  expect(action).toEqual({
+    type: SET_TEXT_FILTER,
+    text
+  })
+});
+
+test('Should setup a set text filter action with no given parameters', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: SET_TEXT_FILTER,
+    text: ''
+  })
+});
+
+test('Should setup a sort by amount action', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type: SORT_BY_AMOUNT
+  })
+});
+
+test('Should setup a sort by date action', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: SORT_BY_DATE
+  })
+});
+
+test('Should setup a set start date action', () => {
+  const startDate = moment();
+  const action = setStartDate(startDate);
+  expect(action).toEqual({
+    type: SET_START_DATE,
+    startDate
+  })
+});
+
+test('Should setup a set end date action', () => {
+  const endDate = moment();
+  const action = setEndDate(endDate);
+  expect(action).toEqual({
+    type: SET_END_DATE,
+    endDate
+  })
+});
